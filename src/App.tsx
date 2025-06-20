4df4647a8bbbac48ac2e29266d243e057002c17d
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-06-20 10:28:53 +0300

    home section + aboutus section

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import background1 from './assets/Group 42.png';
+import background2 from './assets/Cojusna 7 1.png';
+import logo from "./assets/Logo Krov 1.png";
+import heroline from "./assets/Line 3.png"
+import aboutusline from "./assets/Line 2.png"
+import aboutusimage from "./assets/IMG-057cfa42c102590805aac385f53cbbf4-V 1.png"
+import aboutusblueimage from "./assets/Group 43.png"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='Hero'>
+        <div className='Hero-background'>
+          <img src={background1}  className='Hero-background-image1' />
+          <img src={background2}  className='Hero-background-image2' />
+        </div>
+        <div className='Hero-logo'>
+          <div className='Hero-logo-bg'></div>
+          <img src={logo} className='Hero-logo-image' />
+        </div>
+        <div className='Hero-text'>
+          <img src={heroline} className='Hero-text-line' />
+          <h1 className='Hero-text-title-one'>Expertiză în</h1>
+          <h1 className='Hero-text-title-two'>fiecare șurub</h1>
+          <h1 className='Hero-text-title-three'>
+            <span className="bold">Soluții</span> complete pentru <span className="bold">case</span> și <span className="bold">afaceri.</span>
+          </h1>
+          <button className='Hero-text-button'>Produsele Noastre</button>
+        </div>
+        <div className='AboutUs'>
+          <h1 className='AboutUs-title'>Cine <span className="bold-aboutus">Suntem Noi?</span></h1>
+          <h1 className='AboutUs-description-one'>Krov Acoperișuri este o companie fondată și condusă de Friiuc Andrian și Papuc Marcel, profesioniști dedicați în domeniul construcțiilor și sistemelor de acoperiș. Cu sediul juridic în Chișinău, str. Bogdan Voievod 10/1, și biroul nostru operațional pe stradela Studenților 2/4, oferim servicii complete și materiale de înaltă calitate pentru acoperișuri și garduri, pe întreg teritoriul Republicii Moldova.</h1>
+          <h1 className='AboutUs-description-two'>Activitatea noastră se concentrează pe:</h1>
+          <ul className='AboutUs-description-three'>
+            <li>construcția și reparația acoperișurilor,</li>
+            <li>instalarea sistemelor de streașină și scurgere,</li>
+            <li>precum și comercializarea materialelor aferente.</li>
+          </ul>
+          <img src={aboutusline} className='AboutUS-line' />
+          <img src={aboutusimage} className='AboutUS-image' />
+          <img src={aboutusblueimage} className='AboutUS-image-blue' />
+          <button className='AboutUS-button'>Galerie</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
